841a6b1ac6fe2a55d04a8b076113712801f332f9
naŭ glenda <plan9git@proton.me>
2023-09-06 02:22:17 +0200

    Delete archive_test.go
    Signed-off-by: naŭ glenda <plan9git@proton.me>

--- test/archive_test.go
+++ test/archive_test.go
@@ -1,18 +1,1 @@
-package archive_test
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/seekr-osint/wayback-machine-golang/wayback"
-)
-
-func main() {
-	client := http.Client{}
-	url, err := wayback.Archive("github.com/max", &client) // the clint argument can also be nil
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Printf("Archived URL: %s\n", url)
-}
